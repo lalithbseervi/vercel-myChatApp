77b65349527beeaaabc0cac319d36f34e8b88151
Lalith <64013114+lalithbseervi@users.noreply.github.com>
2024-07-28 15:48:42 +0530

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,3 @@
 echo "Executing build_files.sh"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py makemigrations
-python3.10 manage.py migrate
+python3.9 -m pip install -r requirements.txt
 echo "Installed dependencies. Exiting build_files.sh"
